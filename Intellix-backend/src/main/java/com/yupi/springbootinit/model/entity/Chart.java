7bb0934ba19b21f9a55adddf3dd037b7ebf79a6e
Jerrylingj <498539230@qq.com>
2025-01-15 14:05:05 +0800

    创建新表,并调整对应的代码

---
+++ Intellix-backend/src/main/java/com/yupi/springbootinit/model/entity/Chart.java
@@ -0,0 +1,63 @@
+package com.yupi.springbootinit.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 图表信息
+ * @TableName chart
+ */
+@TableName(value ="chart")
+@Data
+public class Chart {
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 分析目标
+     */
+    private String goal;
+
+    /**
+     * 图表数据
+     */
+    private String chartData;
+
+    /**
+     * 图表类型
+     */
+    private String chartType;
+
+    /**
+     * AI生成图表数据
+     */
+    private String genChart;
+
+    /**
+     * AI生成分析结论
+     */
+    private String genResult;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic // 逻辑删除字段
+    private Integer isDelete;
+}
---
+++ Intellix-backend/src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 49853
+* @description 针对表【chart(图表信息)】的数据库操作Service
+* @createDate 2025-01-15 13:45:45
+*/
+public interface ChartService extends IService<Chart> {
+
+}
