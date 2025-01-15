408aecb08111e42b85e6606e2ace3397b4c6e01d
Jerrylingj <498539230@qq.com>
2025-01-15 16:15:04 +0800

    修改前端框架,删除无用的代码

--- Intellix/src/pages/User/Login/index.tsx
+++ Intellix/src/pages/User/Login/index.tsx
@@ -154,8 +154,8 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
-          subTitle={'Ant Design 是西湖区最具影响力的 Web 设计规范'}
+          title="Intellix BI"
+          subTitle={'Intellix BI 是西湖区最具影响力的 Web 设计规范'}
           initialValues={{
             autoLogin: true,
           }}
